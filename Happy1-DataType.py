f4c6e4da1b5a561cbad64a51520c864be7f66aff
happy0524 <jhcha524@gmail.com>
2025-03-09 21:59:07 +0900

    Learning about Python Data Type
    Learn about Python Data Type

---
+++ Happy1-DataType.py
@@ -0,0 +1,90 @@
+# from math import *
+
+"""
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+
+"""
+
+
+
+"""
+
+print("baloon")
+print("kid " * 3)
+print(5<10)
+print(False)
+print(not (5>10))
+
+print("")
+
+a = "멍무"
+b = 4
+
+print("우리 강아지 이름은 " + a + "입니다." + "강아지는 " + str(b) + "살 입니다.") 
+
+print("")
+
+print(type(3))
+print(type(str(3)))
+print(type(3.5))
+
+print("")
+
+print(10-3)
+print(10//3)
+print(2**3)
+
+print("")
+
+"""
+
+
+
+"""
+result = floor(3.14)
+print(result)
+result = ceil(3.14)
+print(result)
+result = sqrt(4)
+print(result)
+result = pow(2,3)
+print(result)
+result = abs(-7)
+print(result)
+"""
+
+"""
+
+import math
+
+result = math.floor(4.99)
+print(result)
+result = math.ceil(3.14)
+print(result)
+result = math.sqrt(16)
+print(result)
+
+print("")
+
+import random 
+from random import *
+
+print(random())
+print(int(random() * 45) + 1)
+
+print("")
+
+print(randrange(1,46))
+print(randint(1,45))
+
+"""
+
+
+from random import *
+
+date = randint(4,28)
+
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정됐습니다.")

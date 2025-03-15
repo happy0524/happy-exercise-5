53c13017091e805408f7943f96e85d02bb1c20e4
happy0524 <jhcha524@gmail.com>
2025-03-15 16:49:48 +0900

    tuple, set practice
    tuple, set practice

--- Happy3-listDIctionrary.py
+++ Happy3-listDIctionrary.py
@@ -46,7 +46,6 @@ print(3 in cab)
 print(77 in cab)
 
 print("p" in "pullup")
-"""
 
 cab = {3: "pu", 100 : "piglet"}
 empty = {}
@@ -58,3 +57,54 @@ print(cab.keys())
 print(cab.values())
 print(cab.items())
 cab.clear()
+
+menu = ("don", "che", "a","ca")
+print(menu[1:3])
+
+(name, age, hobby) = ("피글렛", 20, "코딩")
+print(name)
+
+
+
+print(java & python)
+print(java.intersection(python))
+
+print(java|python)
+print(java.union(python))
+
+print(java - python)
+print(java.difference(python))
+
+java = {"pu", "pig", "ti"}
+python = set(["pu", "igo"])
+go = set()
+
+python.add("pig")
+java.remove("pig")
+
+menu = {"커피" , "우유", "주스"}
+print(menu)
+
+menu = tuple(menu)
+print(type(menu))
+
+"""
+
+from random import *
+
+
+reply = {1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,11:11,12:12,13:13,14:14,15:15,16:16,17:17,18:18,19:19,20:20}
+liss = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+shuffle(liss)
+
+win = sample(liss,4)
+
+print("-- 당첨자 발표 --")
+
+print("치킨 당첨자 : ", win[0])
+print("커피 당첨자 : ", win[1:])
+
+print("-- 축하합니다! --")
+
+
